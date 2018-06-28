92e00572ddb014389ab164b2741af3794924895b
chenqingkun <1431084291@qq.com>
2018-06-28 16:04:58 +0800

    print hello world

---
+++ src/com/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.hello;
+
+/**
+ * Created by Administrator on 2018\6\28 0028.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+    }
+}
